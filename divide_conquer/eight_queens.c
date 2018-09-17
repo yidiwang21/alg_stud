59d2546f08e66d4dfbaa8d21172de83d78b4150b
yidiwang21 <ydwang21@gmail.com>
2018-09-16 22:25:50 -0700

    first row need to fix

--- divide_conquer/eight_queens.c
+++ divide_conquer/eight_queens.c
@@ -4,14 +4,15 @@
 #include<math.h>
 #include<stdbool.h>
 
-#define QUEEN_NUM   8
 #define TRUE        1
 #define FALSE       0
 
 int cnt = 0;
 int *chessboard = NULL;
 
-void Init() {
+// row start from 0, col start from 1
+
+void Init(int QUEEN_NUM) {
     chessboard = (int *)malloc(sizeof(int) * QUEEN_NUM);
     memset(chessboard, 0, sizeof(int) * QUEEN_NUM);
 }
@@ -21,44 +22,56 @@ void placeQueen(int row, int col) {
 }
 
 bool isValid(int row, int col) {
-    for (int i = 0; i < QUEEN_NUM; i++) {
+    for (int i = 0; i < row; i++) {
         if (*(chessboard + i) == col || abs(i - row) == abs(*(chessboard + i) - col))
             return FALSE;
     }
     return TRUE;
 }
 
-void printChessboard() {
+void printChessboard(const int QUEEN_NUM) {
     int k = 0;
     printf("[%d]: \n", cnt++);
     for (int i = 0; i < QUEEN_NUM; i++) {   // scan rows
-        for (k = 0; k < *(chessboard + i); k++) {
-            printf("0 ");
-        }
-        printf("1 ");
-        for (k = *(chessboard + i) + 1; k < QUEEN_NUM; k++) {
-            printf("0 ");
+        for (k = 1; k <= QUEEN_NUM; k++) {
+            if (k == *(chessboard + i))
+                printf("1 ");
+            else
+                printf("0 ");
         }
         printf("\n");
     }
+    // for (int i = 0; i < QUEEN_NUM; i++) {
+    //     printf("%d, %d\n", i, *(chessboard + i));
+    // }
     printf("\n");
 }
 
-void NQueens(int row) {
-    if (row  == QUEEN_NUM - 1) {
-        printChessboard();
+void NQueens(int row, const int QUEEN_NUM) {
+    if (row  == QUEEN_NUM) {
+        printChessboard(QUEEN_NUM);
     }else {
-        for (int j = 0; j < QUEEN_NUM; j++) {    // every col at #row
+        for (int j = 1; j <= QUEEN_NUM; j++) {    // every col at #row
             if (isValid(row, j)) {
                 placeQueen(row, j);
-                NQueens(row + 1);
+                // printf("place at: (%d, %d) | %d\n", row, j, *(chessboard + row));
+                NQueens(row + 1, QUEEN_NUM);
             }
         }
     }
 }
 
 int main(int argc, char const *argv[]) {
-    Init();
-    NQueens(0);
+    if (argc <= 1) {
+        printf("Input format: ./eight_queens QUEEN_NUM\n");
+        return -1;
+    }
+    const int QUEEN_NUM = atoi(argv[1]);
+    if (QUEEN_NUM > 20 || QUEEN_NUM <= 1) {
+        printf("ERROR: QUEEN_NUM must be in range from 2 to 19!\n");
+    }
+
+    Init(QUEEN_NUM);
+    NQueens(1, QUEEN_NUM);
     return 0;
 }
